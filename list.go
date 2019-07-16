3b72bba533b941a431937b171ccf9ced24e696b2
lifeng <beeleelee@outlook.com>
2019-07-16 15:34:50 +0800

    Each loop

--- list.go
+++ list.go
@@ -17,3 +17,9 @@ type EachFn func(v Item, i int, l Lister)
 // type for map function handle
 type MapFn func(v Item, i int, l Lister) Item 
 
+func Each(data Lister, f EachFn){
+	l := data.Len()
+	for i := 0; i < l; i++ {
+		f(data.Get(i), i, data)
+	}
+}
