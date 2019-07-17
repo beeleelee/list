a48e0282fe5d3c12894c7bae00e64a98f203264c
lifeng <beeleelee@outlook.com>
2019-07-17 10:28:25 +0800

    refine on comments

--- list.go
+++ list.go
@@ -1,21 +1,22 @@
 package list
 
-// generic type for list item 
+//Item - generic type for list item 
 type Item interface {}
 
-// interface for list 
+//Lister - interface for list 
 type Lister interface {
 	Len() int 
 	Get(i int) Item 
 	Set(i int, v Item)
 }
 
-// type for each function handle 
+//EachFn  type for each function handle 
 type EachFn func(v Item, i int)
 
-// type for map function handle
+//MapFn type for map function handle
 type MapFn func(v Item, i int) Item 
 
+// Each - each loop handler
 func Each(list Lister, f EachFn){
 	l := list.Len()
 	for i := 0; i < l; i++ {
@@ -23,6 +24,7 @@ func Each(list Lister, f EachFn){
 	}
 }
 
+// Map - map loop handler
 func Map(list Lister, f MapFn) Lister {
 	l := list.Len()
 	mapedList := list 
