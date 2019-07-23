ea4e5fc8e41b0732dd5bdf0277317c92303c8edf
lifeng <beeleelee@outlook.com>
2019-07-23 13:56:26 +0800

    add concurrent test

--- bench_test.go
+++ bench_test.go
@@ -30,5 +30,46 @@ func TestBigList(t *testing.T) {
 	sliceSumEndTime := time.Now()
 	t.Logf("slice sum duration: %v", sliceSumEndTime.Sub(sliceSumStartTime))
 	t.Log(s, sum)
+	//t.Fail()
+}
+
+func TestBigListConcurrent(t *testing.T) {
+	maxSize := 1 << 26
+	t.Log(maxSize)
+	list := List(make([]Item, maxSize))
+	intSlice := make([]int, maxSize)
+	for i, _ := range list {
+		list[i] = i
+		intSlice[i] = i
+	}
+	listSumStartTime := time.Now()
+	goRoutineNum := 10
+	s := 0
+	splitNum := maxSize / goRoutineNum
+	var start, end int
+	for i := 0; i < goRoutineNum; i++ {
+		start = i * splitNum
+		if i + 1 == goRoutineNum {
+			end = maxSize
+		}else{
+			end = (i + 1) * splitNum
+		}
+		go func(l List) {
+			s += (l.Reduce(func(a, b Item) Item {
+				return a.(int) + b.(int)
+			}, nil)).(int)
+		}(list[start:end])
+	}
+	listSumEndTime := time.Now()
+	t.Logf("list sum duration: %v", listSumEndTime.Sub(listSumStartTime))
+	
+	sliceSumStartTime := time.Now()
+	sum := 0
+	for _, v := range intSlice {
+		sum += v
+	}
+	sliceSumEndTime := time.Now()
+	t.Logf("slice sum duration: %v", sliceSumEndTime.Sub(sliceSumStartTime))
+	t.Log(s, sum)
 	t.Fail()
 }
