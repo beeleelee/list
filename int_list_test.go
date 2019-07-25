6d6be2513adda466b8d8e5b9721e057116fd90f5
lifeng <beeleelee@outlook.com>
2019-07-26 00:11:19 +0800

    IntList method Map test

--- int_list_test.go
+++ int_list_test.go
@@ -2,7 +2,7 @@ package list_test
 
 import (
 	"testing"
-	//"reflect"
+	"reflect"
 	. "../list"
 )
 
@@ -11,4 +11,15 @@ func TestIntListEach(t *testing.T) {
 	list.Each(func(v int, i int){
 		t.Log(v, i)
 	})
+}
+
+func TestIntListMap(t *testing.T) {
+	list1 := IntList([]int{2,3,5,7,9,11,13})
+	list2 := list1.Map(func(v, _ int) int {
+		return v + 1
+	})
+	expectedList := IntList([]int{3,4,6,8,10,12,14})
+	if reflect.DeepEqual(list2, expectedList) {
+		t.Errorf("intlist map error: list2 should be %v", expectedList)
+	}
 }
